4fd42f9b92c1e98fc74f714b33a0ef299fafbb54
CaioQC <caioq2004@gmail.com>
2025-07-10 15:13:05 -0300

    Configurando models e migrations , adicionando seeders

--- src/migrations/20250710141716-create-matricula.js
+++ src/migrations/20250710141716-create-matricula.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Matriculas', {
+    await queryInterface.createTable('matriculas', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -11,6 +11,16 @@ module.exports = {
       },
       status: {
         type: Sequelize.STRING
+      },
+      estudante_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "pessoas", key: "id" }
+      },
+      curso_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "cursos", key: "id" }
       },
       createdAt: {
         allowNull: false,
@@ -23,6 +33,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Matriculas');
+    await queryInterface.dropTable('matriculas');
   }
 };
