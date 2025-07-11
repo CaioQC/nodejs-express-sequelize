33bd69a117f2e57f6446ac836c3e3db90d2ebd45
CaioQC <caioq2004@gmail.com>
2025-07-10 21:46:53 -0300

    Adicionando mais métodos na Service e funcionalidades finais

--- src/services/Services.js
+++ src/services/Services.js
@@ -9,6 +9,14 @@ class Services {
         return dataSource[this.model].findAll()
     }
 
+    async pegaUmRegistroPorId(id) {
+        return dataSource[this.model].findByPk(id);
+    }
+
+    async criaRegistro(dadosDoRegistro) {
+        return dataSource[this.model].create(dadosDoRegistro);
+    }
+
     async atualizaRegistro(dadosAtualizados, id){
         const listaRegistroAtualizado = dataSource[this.model].update(dadosAtualizados, { where: { id: id } })
 
@@ -17,6 +25,10 @@ class Services {
         }
 
         return true;
+    }
+
+    async excluiRegistro(id) {
+        return dataSource[this.model].destroy({ where: { id: id } });
     }
 }
 
