c1fe551107d89d1000c565eb77d929653113a8b0
CaioQC <caioq2004@gmail.com>
2025-07-12 13:18:55 -0300

    Adicionando busca de curso por data e pegar matriculas confirmadas

--- src/controllers/MatriculaController.js
+++ src/controllers/MatriculaController.js
@@ -1,11 +1,29 @@
 const Controller = require("./Controller.js")
 const PessoaServices = require("../services/PessoaServices.js")
+const MatriculaServices = require("../services/MatriculaServices.js")
 
 const pessoaServices = new PessoaServices();
+const matriculaServices = new MatriculaServices()
 
 class PessoaController extends Controller {
     constructor(){
         super(pessoaServices)
+    }
+
+    async pegaMatriculaPorEstudante(req, res) {
+        const { estudante_id } = req.params;
+        
+        try {
+            const listaMatriculasPorEstudante = await matriculaServices.pegaEContaRegistros({
+                estudante_id: Number(estudante_id),
+                status: "matriculado"
+            })
+            return res.status(200).json(listaMatriculasPorEstudante);
+        } 
+        
+        catch (erro) {
+            return res.status(500).json({ erro: erro.message })
+        }
     }
 }
 
